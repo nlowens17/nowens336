76099cf27d5b731359b85e7c25366248e10f46ca
Noah Owens <nowens@csumb.edu>
2018-02-18 07:26:04 +0000

    Lab2 second commit

--- portfolio/lab2/index.php
+++ portfolio/lab2/index.php
@@ -3,16 +3,22 @@
     <head>
         <meta charset="utf-8" />
         <title> Lab 2 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
-        
+        <div id= "main">
         <?php
+        include 'inc/functions.php';
+        //$randomValue = rand(0,2);
         
-            function displaySymbol($randomValue) {
-            //$randomValue = rand(0,3);
-            echo $randomValue;
-            
-            switch ($randomValue){
+        $randomValue1 = rand(0,3);
+        $randomValue2 = rand(0,3);
+        $randomValue3 = rand(0,3);
+        
+        /*function displaySymbol($randomValue) {
+            switch ($randomValue) {
                 case 0: $symbol = "seven";
                 break;
                 
@@ -25,42 +31,34 @@
                 case 3: $symbol = "grapes";
                 break;
             }
-            
-            echo "<img src='img/$symbol.png' alt='$symbol' title=\"$symbol\"/>";
-            }
-            
-            function displayPoints ($randomValue1, $randomValue2, $randomValue3) {
-                if ($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3) {
-                switch($randomValue1) {
-                    
-                        case 0: echo "Jackpot!";
-                        $totalpoints = 1000;
-                        break;
-                    
-                        case 1: $totalpoints = 500;
-                        break;
-                    
-                        case 2: $totalpoints = 250;
-                        break;
-                    }
-                    echo "<h2> You won $totalpoints points!</h2>";
+            echo "<img src= 'img/$symbol.png' alt='$symbol' title= '".ucfirst($symbol)."'>";
+        }
+        
+        function displayPoints($randomValue1, $randomValue2, $randomValue3) {
+            echo "<div id='output'>";
+            if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3) {
+                switch ($randomValue1) {
+                    case 0: $totalPoints = 1000;
+                    echo "<h1>Jackpot!</h1>";
+                    break;
+                    case 1: $totalPoints = 500;
+                    break;
+                    case 2: $totalPoints = 250;
+                    break;
                 }
-                else {
-                    echo "Try Again";
-                }
-                echo "</div>";
+                echo "<h2>You won $totalPoints points!</h2>";
             }
-            for ($i = 1; $i <4 ; $i++) {
-                ${"randomValue1" . $i } = rand(0,3 );
-            displaySymbol(${"randomValue" . $i});
+            else {
+                echo "<h3> Try Again!</h3>";
             }
-            displayPoints($randomValue1, $randomValue2, $randomValue3) ;
-            
-            echo "<br /><hr> Value: $randomValue1 $randomValue2 $randomValue3 ";
-            
-            echo"<br /><hr>";
-           
-            ?>
-
+            echo "</div>";
+        }*/
+        play();
+        ?>
+        
+        <form>
+            <input type= "submit" value= "Spin!"/>
+        </form>
+        </div>
     </body>
 </html>
