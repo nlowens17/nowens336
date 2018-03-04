bf6847a47ac5b79762061711a78ed2bdbeef8534
Noah Owens <nowens@csumb.edu>
2018-03-04 05:40:31 +0000

    First commit

--- portfolio/Lab4/api/pixabayAPI.php
+++ portfolio/Lab4/api/pixabayAPI.php
@@ -4,7 +4,7 @@
 function getImageURLs($keyword, $orientation="horizontal") {
     $curl = curl_init();
     curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
+      CURLOPT_URL => "https://pixabay.com/api/?key=8240896-805b6edc2913e731ddb213922&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
---
+++ practice/notes.php
@@ -0,0 +1,24 @@
+/* Primary key
+Creating first table
+    catName - VARCHAR(Similar to a string) - (specify the length) 25
+    catDescription - VARCHAR - 255
+    Category table
+    Save
+Electronics - tvs, audio, equipment, phones, etc
+Video games- consoles, controllers, games
+Sports - balls, rackets, weights, etc
+Computers - Laptops, tablets, desktops
+Books - books, textooks, manuals, magazeins, novels
+Toys - board games, trains
+
+Creating product table
+    product - primary key - aj // !Important!
+    productName - VARCHAR - 25
+    Product description - VARCHAR - 500
+    productImage - VARCHAR(url) - 500
+    (Click go at the top to create new row)
+    price - int (or double) does not have length (only specify lengths for VARCHAR)
+    
+Click structure tab to modify
+    create new row fr category id
+*/
